ac269a2ad7b2aa19bb6df837b4c7c7cf00e3d52c
unknown <599085970@qq.com>
2019-07-29 08:31:33 +0800

    keyboard revieve

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+host = "10.64.83.186"
+port = 5051
+
+if __name__ == '__main__':
+	s = socket.socket()
+	s.bind((host, port))
+	s.listen(5)
+	c, addr = s.accept()
+	while True:
+		buf = c.recv(16)
+		if buf:
+			print(buf)
+		if(b"esc" in buf):
+			break
+	s.close()
