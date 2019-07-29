806091683dae3d54360a0652388a0896e5f6f64a
Tang <renhao.tang@hpe.com>
2019-07-29 16:10:08 +0800

    key bind

--- server_A.py
+++ server_A.py
@@ -1,4 +1,5 @@
 import socket
+from pynput.keyboard import Key, Controller
 host = "10.64.83.12"
 port = 5053
 
@@ -10,7 +11,9 @@ if __name__ == '__main__':
     while True:
         buf = c.recv(16)
         if buf:
-            print(buf)
+            print(buf.decode())
+            keyboard.press(buf.decode())
+            keyboard.release(buf.decode())
         if(b"esc" in buf):
             break
     s.close()
