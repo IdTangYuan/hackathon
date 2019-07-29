c8033e64c319f2f52e20853b716abf47d05bf1d4
Tang <renhao.tang@hpe.com>
2019-07-29 09:39:18 +0800

    change the process

---
+++ test.py
@@ -0,0 +1,17 @@
+from pynput.keyboard import Key, Controller
+import time
+
+keyboard = Controller()
+
+# 按住shift在按a
+with keyboard.pressed(Key.alt):
+    # Key.shift_l, Key.shift_r, Key.shift
+    keyboard.press(Key.space)
+    keyboard.release(Key.space)
+
+# 直接输入Hello World
+keyboard.type('win sublime')
+
+time.sleep(1)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
